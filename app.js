9581148d91d5c1f8f842dc8ccf10faab949aa3cf
samuelM22 <tobbyobioma195@gmail.com>
2024-03-23 06:49:02 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Eden Cloud Tech! We are proud to serve you. Thans for visiting our page'));
+app.get('/', (req, res) => res.send('Welcome to Eden Cloud Tech! We are proud to serve you. Thanks for visiting our page...'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
